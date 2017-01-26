293264f8ba60a66c168131371346bf96f2404174
Thomas Stacey <thomas.stacey@digital.hmrc.gov.uk>
2017-01-26 21:21:55 +0000

    Basic futures

--- src/main/scala/Foochurs.scala
+++ src/main/scala/Foochurs.scala
@@ -1,18 +1,44 @@
-
-
+import scala.concurrent.{Await, Future}
+import scala.util.{Failure, Success}
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 object Foochurs {
 
   def main(args: Array[String]): Unit = {
 
-    val first = slowAddition(1, 1)
-    val second = slowAddition(2, 2)
-    val third = slowerAddition(3, 3)
+    println("")
+    val first = Future {
+      slowAddition(1, 1)
+    }
+    val third = Future {
+      slowerAddition(3, 3)
+    }
+    val second = Future {
+      slowAddition(2, 2)
+    }
 
-    println(s"first: $first")
-    println(s"second: $second")
-    println(s"third: $third")
+    first.onComplete {
+      case Success(firstResult) => println(s"first: $firstResult")
+      case Failure(ex) => println(s"Failed! Reason: ${ex.getMessage}")
+    }
+
+    second.onSuccess {
+      case secondResult => println(s"second: $secondResult")
+    }
+    second.onFailure {
+      case ex => println(s"Failed! Reason: ${ex.getMessage}")
+    }
+
+    third.map {
+      case thirdResult => println(s"third: $thirdResult")
+    } recover {
+      case ex => println(s"Failed! Reason: ${ex.getMessage}")
+    }
+
+    Thread.sleep(4000)
 
   }
+
 
   def slowAddition(a: Int, b: Int): Int = {
       println(s"slowly adding $a & $b")
