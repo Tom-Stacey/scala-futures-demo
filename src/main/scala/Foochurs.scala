5fb82eae8e19874e65bb6b97cfbcdd3ca90ad1e8
Thomas Stacey <thomas.stacey@digital.hmrc.gov.uk>
2017-01-26 18:25:06 +0000

    Non-future code

--- src/main/scala/Foochurs.scala
+++ src/main/scala/Foochurs.scala
@@ -4,7 +4,25 @@ object Foochurs {
 
   def main(args: Array[String]): Unit = {
 
-    println("Hola")
+    val first = slowAddition(1, 1)
+    val second = slowAddition(2, 2)
+    val third = slowerAddition(3, 3)
 
+    println(s"first: $first")
+    println(s"second: $second")
+    println(s"third: $third")
+
+  }
+
+  def slowAddition(a: Int, b: Int): Int = {
+      println(s"slowly adding $a & $b")
+      Thread.sleep(2000)
+      a + b
+  }
+
+  def slowerAddition(a: Int, b: Int): Int = {
+    println(s"more slowly adding $a & $b")
+    Thread.sleep(3000)
+    a + b
   }
 }
